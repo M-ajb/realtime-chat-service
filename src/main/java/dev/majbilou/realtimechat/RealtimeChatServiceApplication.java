f74166a0b9be7190247ac15f721b5357fa8247ab
M-ajb <195341850+M-ajb@users.noreply.github.com>
2026-01-20 21:26:48 +0100

    chore: bootstrap Spring Boot project with Maven and base structure

---
+++ src/main/java/dev/majbilou/realtimechat/RealtimeChatServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.majbilou.realtimechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeChatServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/majbilou/realtimechat/RealtimeChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.majbilou.realtimechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
